0524ee6436dedf15d75f56539ea18868b181431b
seitamuro <seitamuro0210@gmail.com>
2024-06-23 19:01:10 +0900

    Initial commit

---
+++ test/cdk_sns.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSns from '../lib/cdk_sns-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_sns-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSns.CdkSnsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
